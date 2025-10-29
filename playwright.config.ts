5aac37c0cc1e621816f5b5fe105e446cc625a1c2
sharathtn73 <122580945+sharathtn73@users.noreply.github.com>
2025-10-29 18:51:35 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
